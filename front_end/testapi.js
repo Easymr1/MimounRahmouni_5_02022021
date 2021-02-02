e862d47c29516ef789e2b69f37a418453b6cb0f5
Mimoun Rahmouni <mimoun06270@hotmail.fr>
2021-02-02 13:42:50 -0500

    création projet 5

---
+++ front_end/testapi.js
@@ -0,0 +1,29 @@
+let xhr = new XMLHttpRequest();
+
+xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        let response = JSON.parse(this.responseText);
+        console.log(response);
+        for (let i = 0; i < response.length; i++)  {
+            console.log(response[i].name)
+            let parent = document.getElementById("parent").innerHTML +=
+                `<article class = "produit">
+            <h1 class = "title" id = "${response[i].name}">${response[i].name}</h1>
+            <img class = "img_product" src="${response[i].imageUrl}" alt="">
+            <p class = "description" >${response[i].description}</p>
+            <h2 class = "prix" id = "${response[i].price/100}">${response[i].price/100}</h2>
+            <label for="color-select">Choose a vernish:</label>
+            <select name="color" id="color-select">
+            <option value="">Please choose an option</option>
+            <option value="${response[i].varnish[0]}">${response[i].varnish[0]}</option>
+            <option value="${response[i].varnish[1]}">${response[i].varnish[1]}</option>
+            <option value="${response[i].varnish[2]}">${response[i].varnish[2]}</option>
+            </select>
+            <button>Buy</button>
+            </article>`
+        }
+
+    }
+}
+xhr.open("GET", "http://localhost:3000/api/furniture", true);
+xhr.send()
