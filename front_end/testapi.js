cf63c6c92674ae683c549143fb7d28f371f650d7
Mimoun Rahmouni <mimoun06270@hotmail.fr>
2021-02-03 09:12:00 -0500

    test api

--- front_end/testapi.js
+++ front_end/testapi.js
@@ -11,7 +11,7 @@ xhr.onreadystatechange = function() {
             <h1 class = "title" id = "${response[i].name}">${response[i].name}</h1>
             <img class = "img_product" src="${response[i].imageUrl}" alt="">
             <p class = "description" >${response[i].description}</p>
-            <h2 class = "prix" id = "${response[i].price/100}">${response[i].price/100}</h2>
+            <h2 class = "prix" value = "${response[i].price/100}">${response[i].price/100}€</h2>
             <label for="color-select">Choose a vernish:</label>
             <select name="color" id="color-select">
             <option value="">Please choose an option</option>
@@ -19,7 +19,7 @@ xhr.onreadystatechange = function() {
             <option value="${response[i].varnish[1]}">${response[i].varnish[1]}</option>
             <option value="${response[i].varnish[2]}">${response[i].varnish[2]}</option>
             </select>
-            <button>Buy</button>
+            <button id = "addToCart" onclick = "onClick()">Buy</button>
             </article>`
         }
 
@@ -27,3 +27,7 @@ xhr.onreadystatechange = function() {
 }
 xhr.open("GET", "http://localhost:3000/api/furniture", true);
 xhr.send()
+
+function onClick() {
+    document.getElementById("cart").innerHTML = "Merci";
+}
