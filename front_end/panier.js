7642a1330e5888dee0f8dce293a9687272dab447
Mimoun Rahmouni <mimoun06270@hotmail.fr>
2021-02-23 10:34:56 -0500

    récupération produit local storage et création des produit dans le panier

--- front_end/panier.js
+++ front_end/panier.js
@@ -5,7 +5,21 @@ document.querySelector(".cart").textContent = cartNumbers;
 
 product = JSON.parse(product);
 Object.values(product).map(item => {
-    console.log('hello')
+    document.querySelector('.produit').innerHTML += `
+    <div class = 'item'>
+        <div class='img__title'>
+            <img class='img__produit' src =${item.image}>
+            <div class = 'title'>
+                <h3>${item.name}</h3>
+                <p class='numberItems'>Qté : ${item.inCart}</p>
+            </div>
+        </div>
+        <p class ='price'>${item.price}</p>
+    </div>
+    `
+
 })
+
+document.querySelector('.prixTotal').textContent = total;
 
 console.log(product.length);
