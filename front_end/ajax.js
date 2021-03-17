1426361f76ab227932b75b3ca1b306d394d223f7
Mimoun Rahmouni <mimoun06270@hotmail.fr>
2021-03-17 11:20:13 -0400

    Suppresion de l'element de le panier réussi

--- front_end/ajax.js
+++ front_end/ajax.js
@@ -5,3 +5,28 @@ function getElement() {
         .then(articles => articles)
         .catch(error => alert(error));
 }
+
+async function post() {
+
+    await fetch("http://localhost:3000/api/furniture/order", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(cartInformation)
+    }).then(function(response) {
+        return response.json();
+    }).then(data => {
+
+        if (data.orderId !== undefined) {
+            alert('formulaire envoyé !');
+            sessionStorage.setItem('orderId', data.orderId);
+            sessionStorage.setItem('products', JSON.stringify(product));
+            sessionStorage.setItem('Total', total);
+            localStorage.clear();
+            window.location = `confirmation.html?id=${data.orderId}`;
+        } else {
+            alert('Erreur dans le formulaire')
+        };
+    });
+}
--- front_end/panier.js
+++ front_end/panier.js
@@ -14,27 +14,16 @@ const cartInformation = {
 //Création produit du panier
 const creationProduit = () => {;
     product = JSON.parse(product);
-
+    const caseProduit = document.querySelector('.produit');
+    let arrayProduit = [];
     if (product !== null) {
         Object.values(product).map(item => {
-            document.querySelector('.produit').innerHTML += `
-    <div class = 'item'>
-        <div class='img__title'>
-            <img class='img__produit' src =${item.image}>
-            <div class = 'title'>
-                <h3>${item.name}</h3>
-                <p class='numberItems'>Qté : <button class='moins'><i class="fas fa-minus"></i></button> <strong>${item.inCart}</strong> <button class ='plus'><i class="fas fa-plus"></i></button> <button class ='supprimer'>Supprimer</button></p>
-            </div>
-        </div>
-        <p class ='price'>${item.price}</p>
-    </div>
-    `;
-            document.querySelector('.prixTotal').textContent = total + ' €';
-            cartInformation.products.push(item.id); // Envoie id pour la requet push
+            arrayProduit.push(item);
         })
+        newProduit(arrayProduit, caseProduit);
     } else {
         //Si le panier et vide le formulaire ne s'affiche pas//
-        document.querySelector('.produit').innerHTML += `
+        caseProduit.innerHTML += `
         <div class = 'panierVide'>
         <h2>Votre panier et vide</h2>
         </div>
@@ -42,18 +31,78 @@ const creationProduit = () => {;
         let formulaire = document.querySelector('.formulaire');
         formulaire.style.display = "none";
         document.querySelector('.prixTotal').textContent = 0 + ' €';
+
     }
-    let id = Object.keys(product);
-
-    let supprimer = document.querySelectorAll('.supprimer');
-    supprimer.forEach(item => addEventListener('click', async() => {
-        console.log()
-    }));
-
-
 
 
 }
+
+
+const newProduit = (arrayProduit, caseProduit) => {
+    for (let i = 0; i < arrayProduit.length; i++) {
+        caseProduit.innerHTML += `
+    <div class = 'item'>
+        <div class='img__title'>
+            <img class='img__produit' src =${arrayProduit[i].image}>
+            <div class = 'title'>
+                <h3>${arrayProduit[i].name}</h3>
+                <p class='numberItems'>Qté : <button class='moins'><i class="fas fa-minus"></i></button> <strong class='nbrProduit${i}'>${arrayProduit[i].inCart}</strong> <button class ='plus'><i class="fas fa-plus"></i></button> <button class ='supprimer' id='${arrayProduit[i].id}'>Supprimer</button></p>
+            </div>
+        </div>
+        <p class ='price'>${arrayProduit[i].price}</p>
+    </div>
+    `;
+
+
+
+
+        supprimerElement(arrayProduit);
+
+        document.querySelector('.prixTotal').textContent = total + ' €';
+        cartInformation.products.push(arrayProduit[i].id);
+    }
+
+}
+
+
+
+
+const supprimerElement = (product) => {
+    let btn = document.querySelectorAll('.supprimer');
+    btn.forEach(item => item.addEventListener('click', () => {
+        let result = product.filter(un => un.id !== item.id);
+        localStorage.removeItem('producte');
+        localStorage.removeItem('Total');
+        result.forEach(item => {
+
+            let articleCart = localStorage.getItem(`producte`);
+            articleCart = JSON.parse(articleCart);
+            console.log(articleCart);
+            console.log(item)
+            if (articleCart === null) {
+                articleCart = {};
+            }
+            if (articleCart[item.id] === undefined) {
+                articleCart[item.id] = item;
+            }
+
+            let total = localStorage.getItem('Total');
+            total = total += item.price;
+
+
+            localStorage.setItem('Total', total);
+            localStorage.setItem(`producte`, JSON.stringify(articleCart));
+
+
+
+        })
+        location.reload();
+
+    }))
+
+
+};
+
 
 
 //Interactcion avec le forumulaire//
@@ -75,24 +124,18 @@ const dataRetouner = () => {
             email: "",
         }
 
-
-
         if (email.match(/\S+@\S+\.\S+/) && email !== "") {
             cartInformation.contact.email = email;
             document.querySelector('.email').textContent = ''
-
         } else {
             document.querySelector('.email').textContent = 'Email incorrect';
         }
-
         if (lastName.match(/\b[^\d\W]+\b/) && lastName !== "") {
             cartInformation.contact.lastName = lastName;
             document.querySelector('.nom').textContent = ''
-
         } else {
             document.querySelector('.nom').textContent = 'Nom incorrect'
         }
-
         if (firstName.match(/\b[^\d\W]+\b/) && firstName !== "") {
             cartInformation.contact.firstName = firstName;
             document.querySelector('.prenom').textContent = ''
@@ -100,7 +143,6 @@ const dataRetouner = () => {
         } else {
             document.querySelector('.prenom').textContent = 'Prénom incorrect'
         }
-
         if (city.match(/\b[^\d\W]+\b/) && city !== "") {
             cartInformation.contact.city = city;
             document.querySelector('.ville').textContent = ''
@@ -108,57 +150,23 @@ const dataRetouner = () => {
         } else {
             document.querySelector('.ville').textContent = 'Ville incorrect'
         }
-
         if (address !== "") {
             cartInformation.contact.address = address;
             document.querySelector('.adresse').textContent = ''
-
         } else {
             document.querySelector('.adresse').textContent = 'Address incorrect'
-
         }
 
         //Vérification de tout les condition avant Envoie des donner à l'API//
-        if (email.match(/\S+@\S+\.\S+/) && email !== "" && lastName.match(/\b[^\d\W]+\b/) && lastName !== "" && firstName.match(/\b[^\d\W]+\b/) && firstName !== "" && city.match(/\b[^\d\W]+\b/) && city !== "" && address !== "") {
+        if (email.match(/\S+@\S+\.\S+/) && email !== "" &&
+            lastName.match(/\b[^\d\W]+\b/) && lastName !== "" &&
+            firstName.match(/\b[^\d\W]+\b/) && firstName !== "" &&
+            city.match(/\b[^\d\W]+\b/) && city !== "" &&
+            address !== "") {
             post();
         }
-
-
-
-
     })
-
-
 };
-
-
-
-
-
-async function post() {
-
-    await fetch("http://localhost:3000/api/furniture/order", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(cartInformation)
-    }).then(function(response) {
-        return response.json();
-    }).then(data => {
-
-        if (data.orderId !== undefined) {
-            alert('formulaire envoyé !');
-            sessionStorage.setItem('orderId', data.orderId);
-            sessionStorage.setItem('products', JSON.stringify(product));
-            sessionStorage.setItem('Total', total);
-            localStorage.clear();
-            window.location = `confirmation.html?id=${data.orderId}`;
-        } else {
-            alert('Erreur dans le formulaire')
-        };
-    });
-}
 
 creationProduit();
 dataRetouner();
